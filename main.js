72b45e7f9914d20ad7b4bbfd6d56bc36d7ce387a
muhamadkafiludin <muhamadkafiludin@gmail.com>
2023-09-27 12:46:21 +0700

    add javascript alert

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
-// alert('hey')
-// prompt('masukan usia anda?')
+// alert('hey') let usia=prompt('masukan usia anda?') alert('usia anda adalah '
+// + usia) let usia = 19 usia =18 console.log('heyho usia kamu adalah' + usia)
+let nama = ('kafil')
 let usia = 19
-usia =18
-console.log('heyho usia kamu adalah' +  usia)
+let tinggiBadan = 165
+let beratBadan
+let pacar = null
+
+beratBadan = 58
+ if (pacar == null){
+    pacar = 'belum punya'
+ } else {
+    pacar = 'udah punya'
+ } 
+
+// switch (pacar) {
+//     case 1:
+//         pacar = 'punya 1 aja'
+//         break
+//     case 2:
+//         pacar = 'punya pacar 2'
+//         break
+//     default:
+//         pacar = 'belum punya'
+//         break
+// }
+ 
+let saldoAwal = 50000
+let saldoTambahan = 80000
+const saldoAkhir = saldoAwal + saldoTambahan
+
+alert(
+    `nama saya ${nama} usia saya itu ${usia} tinggi badan saya adalah ${tinggiBadan}cm berat badan saya ${beratBadan}kg dan pacar saya ${pacar}`
+)
+alert(
+    `saldo awal saya sebesar RP.${saldoAwal} & saldo tambahan saya sebesar RP.${saldoTambahan} jadi total saldo yg aku miliki sebanyak RP.${saldoAkhir}`
+)
